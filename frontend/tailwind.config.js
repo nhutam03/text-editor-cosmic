072ec7bb73c0cd3106da48856ec3216ff910f62b
nhutam03 <nhutam050@gmail.com>
2025-01-24 15:50:53 +0700

    update tailwindcss & daisyui

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,5 +1,9 @@
 module.exports = {
-  content: ['./src/**/*.{html,js,ts,jsx,tsx}'],
+  content: [
+    "./*.html", // Nếu file HTML của bạn nằm ở root
+    "./*.js", // Nếu bạn sử dụng file JavaScript cho giao diện
+    "./renderer/**/*.{html,js}", // Nếu bạn sử dụng file JavaScript cho
+  ],
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
@@ -8,5 +12,10 @@ module.exports = {
   variants: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('daisyui'),
+  ],
+  daisyui: {
+    themes: ["light", "dark"], // Bạn có thể định nghĩa các theme tại đây
+  },
 }
